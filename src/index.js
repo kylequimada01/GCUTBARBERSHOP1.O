9be7c0be561027f2afc1a72595a0bd07b1606a1b
Stefan Gogov <stefangogov@proton.me>
2024-09-02 22:17:36 +0300

    chore: Update npm dependency to axios 1.7.7

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 const mongoose = require('mongoose');
+const axios = require('axios'); // Add axios import
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
+
+// Reloader Function
+const url = `${process.env.BASE_API_URL}/health`; // Replace with your Render URL
+const interval = 50000; // Interval in milliseconds (50 seconds)
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      logger.info(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch((error) => {
+      logger.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+function startReloader() {
+  setInterval(reloadWebsite, interval);
+}
 
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
+    startReloader(); // Start the reloader once the server is up
   });
 });
 
